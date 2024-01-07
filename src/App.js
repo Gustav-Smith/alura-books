82dd2cab03e3d6b5319d0c26260de14259b35cee
Gustav-Smith <gustavojeanpt@gmail.com>
2024-01-07 16:58:12 -0300

    feat: adding state input to react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import './App.css';
 import Header from './componentes/Header';
+import Pesquisa from './componentes/Pesquisa';
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
     background-image: linear-gradient(90deg, #002F22 35%, #326589);
-  
-
-  li {
-    list-style: none;
-  }
 `
 
 function App() {
   return (
     <AppContainer>
       <Header />
+      <Pesquisa />
     </AppContainer>
   );
 }
