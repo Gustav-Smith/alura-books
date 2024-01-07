9fcb01f34a2ea04e565700addebe913385b874b0
Gustav-Smith <gustavojeanpt@gmail.com>
2024-01-06 22:54:15 -0300

    Getting strated with react

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,19 @@
+import './estilo.css'
+import logo from '../../img/logo.svg'
+
+function Logo() {
+  return (
+    <div className='logo'>
+      <img
+        src={logo}
+        alt='logo do site'
+        className='logo-img'
+      ></img>
+      <p> <strong>Alura</strong>Books</p>
+    </div>
+  )
+}
+
+export default Logo
+
+
